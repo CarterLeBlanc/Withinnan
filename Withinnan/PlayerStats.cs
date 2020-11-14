c6c2e29f99d3d3f0cba026ff1336c969fcdbd9a9
Carter LeBlanc <cartermleblanc@gmail.com>
2020-11-13 19:49:06 -0600

    Added starting classes.

---
+++ Withinnan/PlayerStats.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Withinnan
+{
+    class PlayerStats
+    {
+        protected int _playerStrength = 0;
+        protected int _playerIntelligence = 0;
+        protected int _playerCharisma = 0;
+
+        public int GetStrength()
+        {
+            return _playerStrength;
+        }
+
+        public void SetStrength(int strength)
+        {
+            _playerStrength += strength;
+        }
+
+        public int GetIntelligence()
+        {
+            return _playerIntelligence;
+        }
+
+        public void SetIntelligence(int intelligence)
+        {
+            _playerIntelligence += intelligence;
+        }
+
+        public int GetCharisma()
+        {
+            return _playerCharisma;
+        }
+
+        public void SetCharisma(int charisma)
+        {
+            _playerCharisma += charisma;
+        }
+    }
+}
---
+++ Withinnan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Withinnan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
